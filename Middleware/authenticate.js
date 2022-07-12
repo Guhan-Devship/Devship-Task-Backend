635df7ff920108dacdfa5bf6df729ac402b7857a
Guhan <you@example.com>
2022-07-12 11:22:55 +0530

    changes

--- Middleware/authenticate.js
+++ Middleware/authenticate.js
@@ -1,20 +1,19 @@
 const req = require("express/lib/request");
-const jwt =require("jsonwebtoken")
+const jwt = require("jsonwebtoken");
 
 function authenticate(req, res, next) {
-    // Check token present in header
-    if (req.headers.authorization) {
-      let decode = jwt.verify(req.headers.authorization, 'KeyCode');
-      if (decode) {
-        req.userId = decode.id;
-        next();
-      } else {
-        res.status(401).json({ message: 'Unauthorized' });
-      }
+  // Check token present in header
+  if (req.headers.authorization) {
+    let decode = jwt.verify(req.headers.authorization, "KeyCode");
+    if (decode) {
+      req.userId = decode.id;
+      next();
     } else {
-      res.status(401).json({ message: 'Unauthorized' });
-    }    
+      res.status(401).json({ message: "Unauthorized" });
+    }
+  } else {
+    res.status(401).json({ message: "Unauthorized" });
   }
+}
 
-
-module.exports=authenticate;
+module.exports = authenticate;
--- controllers/Category.js
+++ controllers/Category.js
@@ -1,53 +1,76 @@
-const Category =require('../model/Category')
-const fs = require('fs');
+const Category = require("../model/Category");
+const fs = require("fs");
+const multer = require("multer");
 
-const createNew =async(req,res)=>{
-    try {
-        const List = new Category({
-            title:req.body.title,
-            image:req.body.image,
+const fileStorage = multer.diskStorage({
+  destination: (req, res, cb) => {
+    cb(null, "./category");
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + "--" + file.originalname);
+  },
+});
+
+const upload = multer({
+  storage: fileStorage,
+}).single("image");
+
+const createNew = async (req, res) => {
+  upload(req, res, function (err) {
+    if (err) {
+      console.log(err);
+    } else {
+      let image;
+
+      if (req.file) {
+        image = req.file.path.replace(/\\/g, "/");
+      }
+
+      const newCategory = new Category({
+        title: req.body.title,
+        image,
+      });
+      newCategory
+        .save()
+        .then(() => {
+          res.json({ message: "created" });
         })
-        const createData=await List.save();
-        if(createData){
-            res.send({
-                message:"added",
-                createData})
-        }
-    } catch (error) {
-        console.log(error)
+        .catch((err) => {
+          res.send(err);
+        });
     }
-}
+  });
+};
 
-  const getCategory =async(req,res)=>{
-    try {
-        const product =await Category.find({});
-        if(product){
-            res.json(product)
-        }
-    } catch (error) {
-        console.log(error)
+const getCategory = async (req, res) => {
+  try {
+    const product = await Category.find({});
+    if (product) {
+      res.json(product);
     }
-    
-}
-const getProductId =async(req,res)=>{
-    try {
-        const user =await Category.findById(req.params.id);
-        if(user){
-            res.json(user)
-        }
-    } catch (error) {
-        console.log(error)
+  } catch (error) {
+    console.log(error);
+  }
+};
+const getProductId = async (req, res) => {
+  try {
+    const user = await Category.findById(req.params.id);
+    if (user) {
+      res.json(user);
     }
-}
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-const deleteCategory=async (req,res)=>{
-    try {
-        const List =  await Category.findByIdAndDelete(req.params.id)
-        if(List){
-            res.send("deleted Successfully")
-        }
-    } catch (error) {
-        console.log(error)
+const deleteCategory = async (req, res) => {
+  try {
+    const List = await Category.findByIdAndDelete(req.params.id);
+    if (List) {
+      res.send("deleted Successfully");
     }
-}
-module.exports={createNew,getCategory,deleteCategory,getProductId};
+  } catch (error) {
+    console.log(error);
+  }
+};
+module.exports = { createNew, getCategory, deleteCategory, getProductId };
